cc792b3d9419ef00b0c87920d0903884f1f3f5e1
sofiane lasoa <sofiane.lasoa.etu@univ-lille.fr>
2022-11-17 23:50:30 +0100

    Update SignUp back/front

--- server/routers/authRouter.js
+++ server/routers/authRouter.js
@@ -8,7 +8,8 @@ router.post("/signup", async (req, res) => {
         let {email, pseudo, password1, password2} = req.body;
         const emailExist = await pool.query(`select * from users where email=$1`, [email]);
         const pseudoExist = await pool.query(`select * from users where pseudo=$1`, [pseudo]);
-        if(emailExist.rowCount === 0 || pseudoExist.rowCount === 0){
+        if(emailExist.rowCount === 0 && pseudoExist.rowCount === 0){
+            console.log(pseudo.rows);
             if(password1 === password2 && password1.length >= 7){
                 let hashPass = await bcrypt.hash(password1, 10);
                 let insertUser = await pool.query(`insert into users (email, pseudo, password) values ($1, $2, $3) returning *`, [email, pseudo, hashPass],
@@ -22,11 +23,11 @@ router.post("/signup", async (req, res) => {
                     res.send({loggedIn: false, error: "mot de passe differents"});
                 }else{
                     res.send({loggedIn: false, error: "mot de passe trop cours"});
-                }
+                } 
             }
         }else{
             if(emailExist.rowCount > 0) {
-                res.send({loggedIn: false, error:"email deja utilisé"});
+                res.send({loggedIn: false, error:"email deja utilisé"}); 
             }else{
                 if(pseudoExist.rowCount > 0) res.send({loggedIn: false, error:"pseudo déja utilisé"});
             }
